5454114d40c6e57f24335fa3784ce7186f0d340f
Kolin Guo <kolguo@ucdavis.edu>
2019-07-09 15:30:59 -0700

    Update download dataset script

--- download_dataset.sh
+++ download_dataset.sh
@@ -8,44 +8,52 @@ if [ ! -d "./data/zips" ] ; then
 fi
 cd ./data/zips
 
-ZIPNAME="Dataset 1a Development_train.zip"
+ZIPNAME="Dataset 1a Development_train"
 UNZIPPATH="../"
-if [ ! -f "./$ZIPNAME" ] ; then
+if [ ! -f "./$ZIPNAME.zip" ] ; then
         echo -e "Downloading $ZIPNAME [35.3 GB]"
         wget -c -O "$ZIPNAME" https://zenodo.org/record/1470797/files/Dataset%201a%20Development_train.zip?download=1
-        
-        echo -e "\nUnzipping dataset"
-        unzip "$ZIPNAME" -d "$UNZIPPATH"
 fi
 
-ZIPNAME="Dataset 1b Development_validation.zip"
+if [ ! -d "$UNZIPPATH/$ZIPNAME" ] ; then
+        echo -e "\nUnzipping dataset $ZIPNAME"
+        unzip "$ZIPNAME" -d "$UNZIPPATH/$ZIPNAME"
+fi
+
+ZIPNAME="Dataset 1b Development_validation"
 UNZIPPATH="../"
-if [ ! -f "./$ZIPNAME" ] ; then
+if [ ! -f "./$ZIPNAME.zip" ] ; then
         echo -e "Downloading $ZIPNAME [3.9 GB]"
         wget -c -O "$ZIPNAME" https://zenodo.org/record/1470797/files/Dataset%201b%20Development_validation.zip?download=1
-        
-        echo -e "\nUnzipping dataset"
-        unzip "$ZIPNAME" -d "$UNZIPPATH"
 fi
 
-ZIPNAME="Dataset 2 Hold-out.zip"
+if [ ! -d "$UNZIPPATH/$ZIPNAME" ] ; then
+        echo -e "\nUnzipping dataset $ZIPNAME"
+        unzip "$ZIPNAME" -d "$UNZIPPATH/$ZIPNAME"
+fi
+
+ZIPNAME="Dataset 2 Hold-out"
 UNZIPPATH="../"
-if [ ! -f "./$ZIPNAME" ] ; then
+if [ ! -f "./$ZIPNAME.zip" ] ; then
         echo -e "Downloading $ZIPNAME [41.2 GB]"
         wget -c -O "$ZIPNAME" https://zenodo.org/record/1470797/files/Dataset%202%20Hold-out.zip?download=1
-        
-        echo -e "\nUnzipping dataset"
-        unzip "$ZIPNAME" -d "$UNZIPPATH"
 fi
 
-ZIPNAME="Dataset 3 CERAD-like hold-out.zip"
+if [ ! -d "$UNZIPPATH/$ZIPNAME" ] ; then
+        echo -e "\nUnzipping dataset $ZIPNAME"
+        unzip "$ZIPNAME" -d "$UNZIPPATH/$ZIPNAME"
+fi
+
+ZIPNAME="Dataset 3 CERAD-like hold-out"
 UNZIPPATH="../"
-if [ ! -f "./$ZIPNAME" ] ; then
+if [ ! -f "./$ZIPNAME.zip" ] ; then
         echo -e "Downloading $ZIPNAME [26.3 GB]"
         wget -c -O "$ZIPNAME" https://zenodo.org/record/1470797/files/Dataset%203%20CERAD-like%20hold-out.zip?download=1
-        
-        echo -e "\nUnzipping dataset"
-        unzip "$ZIPNAME" -d "$UNZIPPATH"
+fi
+
+if [ ! -d "$UNZIPPATH/$ZIPNAME" ] ; then
+        echo -e "\nUnzipping dataset $ZIPNAME"
+        unzip "$ZIPNAME" -d "$UNZIPPATH/$ZIPNAME"
 fi
 
 ZIPNAME="Tiles.zip"
@@ -54,7 +62,7 @@ if [ ! -f "./$ZIPNAME" ] ; then
         echo -e "Downloading $ZIPNAME [3.3 GB]"
         wget -c -O "$ZIPNAME" https://zenodo.org/record/1470797/files/Tiles.zip?download=1
         
-        echo -e "\nUnzipping dataset"
+        echo -e "\nUnzipping dataset $ZIPNAME"
         unzip "$ZIPNAME" -d "$UNZIPPATH"
 fi
 
