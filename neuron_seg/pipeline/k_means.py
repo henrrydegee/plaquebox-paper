ebbe633aa5ac196fbb421221a473b1a009c14be6
xu1998hz <wedxu@ucdavis.edu>
2019-08-13 11:17:31 -0700

    update k-means photos on non-segmented results

--- neuron_seg/pipeline/k_means.py
+++ neuron_seg/pipeline/k_means.py
@@ -6,6 +6,19 @@ from sklearn.cluster import MiniBatchKMeans, KMeans
 from sklearn.metrics.pairwise import pairwise_distances_argmin
 # unit testing using print
 
+# functions to pick mouse movements and gather x,y coordinates
+def on_pick(event):
+    artist = event.artist
+    xmouse, ymouse = event.mouseevent.xdata, event.mouseevent.ydata
+    x, y = artist.get_xdata(), artist.get_ydata()
+    ind = event.ind
+    print('Artist picked:', event.artist)
+    print('{} vertices picked'.format(len(ind)))
+    print('Pick between vertices {} and {}'.format(min(ind), max(ind)+1))
+    print('x, y of mouse: {:.2f},{:.2f}'.format(xmouse, ymouse))
+    print('Data point:', x[ind[0]], y[ind[0]])
+    print
+
 image = np.array(Image.open("post_threshold_morphology_170.png"),dtype=np.uint8)
 datapoints = np.where(image != 0)
 x_coords = list(datapoints[0])
@@ -14,8 +27,12 @@ y_coords = list(datapoints[1])
 # generate all the datapoints coordinates
 coords = np.array(list((zip(x_coords,y_coords))))
 print(coords)
+
+# Assign cluster centers at beginning
+# init_set = np.array([[811.471, -93.067], [734.521, -884.203]], np.float64)
+
 # compute clustering with Means
-k_means = KMeans(init='k-means++', n_clusters=2, n_init=10)
+k_means = KMeans(init="k-means++", n_clusters=2, n_init=10)
 k_means.fit(coords)
 
 # plot result
@@ -40,6 +57,17 @@ ax.set_title('KMeans')
 ax.set_xticks(())
 ax.set_yticks(())
 
+# Design a datapoint gathering interface to get two initial cluster centers
+# plt.title("All the pixel values displayed in x,y coordinates")
+
+# fig, ax = plt.plot()
+# select two points at maximum
+# tolerance = 2
+# ax.plot(range(2), "ro-", picker=tolerance)
+
+# fig.canvas.callbacks.connect('pick_event', on_pick)
+
 plt.show()
+
 
 
